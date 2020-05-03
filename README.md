03402354d9292da77a6dd800c81099747bcaf21b
Sergei S <div@altertech.com>
2020-05-03 15:57:28 +0200

    import

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # grafana-fes-datasource
-Grafana datasource for Finac Enterprise Server
+
+Grafana datasource for [Finac Enterprise Server](https://www.altertech.com/products/fes/)
+
+(can work with custom [Finac](https://github.com/alttch/finac) servers as well)
+
+## Installation
+
+(become a superuser)
+
+```
+cd /var/lib/grafana/plugins
+git clone https://github.com/alttch/grafana-fes-datasource
+npm i
+npm run build
+systemctl restart grafana-server
+```
+
+## Data source configuration
+
+* Create new data source in Grafana, choose FesDS plugin
+* Use http(s)://yourservername as server URL (no leading slash, no URIs)
+* Enter FES server API key (if set)
+
+## Usage
+
+### Simple SELECT
+
+The data source supports all Finac query commands, e.g.
+
+```
+SELECT account_balance(account='myaccount')
+```
+
+### Switching base currency on-the-flow
+
+* create Grafana variable, named e.g. *base*, put there EUR,USD,CZK as the
+* values.
+
+* then use variable in queries (don't forget about quotes):
+
+```
+SELECT account_balance(account='myaccount', base="${base}")
+```
+
+### Time-based SELECT
+
+Use Grafana global time variables to specify time range, e.g.
+
+```
+SELECT account_balance_range(account='myaccount', start=${__from}, end=${__to})
+```
+
+As time variables are integer numbers, quotes are not required.
+
+## Troubleshooting
+
+### Range charts are slow
+
+Increase range step, e.g.:
+
+```
+SELECT account_balance_range(account='myaccount', start=${__from}, end=${__to}, step=7)
+```
---
+++ src/datasource.js
@@ -0,0 +1,177 @@
+export class GenericDatasource {
+  constructor(instanceSettings, $q, backendSrv, templateSrv) {
+    this.type = instanceSettings.type;
+    this.url = instanceSettings.url;
+    this.name = instanceSettings.name;
+    this.apikey = instanceSettings.jsonData.apikey;
+    this.q = $q;
+    this.backendSrv = backendSrv;
+    this.templateSrv = templateSrv;
+    this.withCredentials = instanceSettings.withCredentials;
+    if (
+      typeof instanceSettings.basicAuth === 'string' &&
+      instanceSettings.basicAuth.length > 0
+    ) {
+      this.headers['Authorization'] = instanceSettings.basicAuth;
+    }
+  }
+
+  buildQueryParameters(options) {
+    //remove placeholder targets
+    options.targets = _.filter(options.targets, target => {
+      return target.target !== 'select metric';
+    });
+
+    var targets = _.map(options.targets, target => {
+      return {
+        target: this.templateSrv.replace(
+          target.target,
+          options.scopedVars,
+          'regex'
+        ),
+        refId: target.refId,
+        hide: target.hide,
+        type: target.type || 'timeserie'
+      };
+    });
+
+    options.targets = targets;
+
+    return options;
+  }
+
+  query(options) {
+    var query = this.buildQueryParameters(options);
+    query.targets = query.targets.filter(t => !t.hide);
+    var q = [];
+    options['targets'].forEach(option => {
+      q.push(option.target);
+    });
+    if (q) {
+      return this.call_query(q).then(result => {
+        result.forEach(r => {
+          if ('error' in r) {
+            throw r['error'];
+          }
+        });
+        return {
+          status: 'success',
+          data: result
+        };
+      });
+    }
+  }
+
+  testDatasource() {
+    return this.call('get_version')
+      .then(result => {
+        return {
+          status: 'success',
+          message: 'Data source is working',
+          title: 'Success'
+        };
+      })
+      .catch(err => {
+        return {
+          status: 'error',
+          message: err
+        };
+      });
+  }
+
+  _uuidv4() {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(
+      c
+    ) {
+      var r = (dt + Math.random() * 16) % 16 | 0;
+      dt = Math.floor(dt / 16);
+      return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+    });
+    return uuid;
+  }
+
+  _prepare_call_params(params) {
+    var p = params ? params : {};
+    if (this.apikey) {
+      p['_k'] = this.apikey;
+    }
+    return p;
+  }
+
+  call_query(q) {
+    return new Promise((resolve, reject) => {
+      fetch(this.url + '/query?grafana=1', {
+        method: 'POST',
+        body: JSON.stringify(q),
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Auth-Key': this.apikey
+        },
+        redirect: 'error'
+      })
+        .then(function(response) {
+          if (response.ok) {
+            response
+              .json()
+              .then(data => {
+                resolve(data);
+              })
+              .catch(err => {
+                reject('Invalid server response');
+              });
+          } else {
+            reject(`Server error ${response.status}`);
+          }
+        })
+        .catch(err => {
+          reject('Server error');
+        });
+    });
+  }
+
+  call(func, params) {
+    var id = this._uuidv4();
+    return new Promise((resolve, reject) => {
+      var payload = {
+        jsonrpc: '2.0',
+        method: func,
+        params: this._prepare_call_params(params),
+        id: id
+      };
+      fetch(this.url + '/jrpc', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        redirect: 'error',
+        body: JSON.stringify(payload)
+      })
+        .then(function(response) {
+          if (response.ok) {
+            response
+              .json()
+              .then(data => {
+                if (
+                  !'id' in data ||
+                  data.id != id ||
+                  (!'result' in data && !'error' in data)
+                ) {
+                  reject('Invalid server response');
+                } else if ('error' in data) {
+                  reject(data.error.message);
+                } else {
+                  resolve(data.result);
+                }
+              })
+              .catch(err => {
+                reject('Invalid server response');
+              });
+          } else {
+            reject(`Server error ${response.status}`);
+          }
+        })
+        .catch(err => {
+          reject('Server error');
+        });
+    });
+  }
+}
